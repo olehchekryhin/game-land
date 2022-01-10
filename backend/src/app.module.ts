b8bc8b6b241e93d49631dba36154b5fc590738e2
Oleg Chekryhin <Oleh.X.Chekryhin@aib.ie>
2022-01-10 18:44:35 +0100

    Update folder structure

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,11 +1,18 @@
 import { Module } from '@nestjs/common';
+import { ServeStaticModule } from '@nestjs/serve-static';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ChatGateway } from "./chat.gateway";
 import { GameModule } from "./components/game/game.module";
+import { join } from 'path';
 
 @Module({
-  imports: [GameModule],
+  imports: [
+      GameModule,
+      ServeStaticModule.forRoot({
+        rootPath: join(__dirname, '../../../', 'frontend/build'),
+      }),
+  ],
   controllers: [AppController],
   providers: [AppService, ChatGateway],
 })
