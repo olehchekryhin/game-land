4b6ef25a618184f0c680f4ff9ca32671e81de202
Oleg Chekryhin <Oleh.X.Chekryhin@aib.ie>
2022-01-10 21:42:13 +0100

    code

--- frontend/src/services/socket.js
+++ frontend/src/services/socket.js
@@ -1,5 +1,5 @@
 import { io } from "socket.io-client";
-const ENDPOINT = "wss://stark-castle-88218.herokuapp.com//";
+const ENDPOINT = "wss://stark-castle-88218.herokuapp.com/";
 const socket = io(ENDPOINT, { transports: ['websocket', 'polling', 'flashsocket'] });
 
 export default socket;
